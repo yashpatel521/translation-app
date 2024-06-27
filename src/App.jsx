a176a9be8b3fe61b99fabe6fbc9be9cd321d700b
Yash Patel <yash1451999@gmail.com>
2024-06-27 15:21:22 -0400

    Backend Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import OpenAI from "openai";
 import { BeatLoader } from "react-spinners";
 
 const App = () => {
-  const [formData, setFormData] = useState({ language: "Hindi", message: "" });
+  const [formData, setFormData] = useState({
+    languageId: "",
+    modelId: "",
+    message: "",
+  });
   const [error, setError] = useState("");
   const [showNotification, setShowNotification] = useState(false);
-  const [correctedText, setCorrectedText] = useState("");
-  const [translation, setTranslation] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [detectedLanguage, setDetectedLanguage] = useState("");
-
-  const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_KEY,
-    dangerouslyAllowBrowser: true,
+  const [result, setResult] = useState({
+    correctedText: "",
+    translation: "",
+    detectedLanguage: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const [options, setOptions] = useState({ languages: [], models: [] });
+
+  const fetchData = async (endpoint) => {
+    try {
+      const url = `${import.meta.env.VITE_BACKEN_URL}/${endpoint}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error(`Error fetching ${endpoint}:`, err);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      setOptions({
+        languages: (await fetchData("language")).data,
+        models: (await fetchData("model")).data,
+      });
+    })();
+  }, []);
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     setError("");
-  };
-
-  const detectLanguage = async (text) => {
-    const response = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `Detect the language of this text: ${text}`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-      temperature: 0.3,
-      max_tokens: 100,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-    return response.choices[0].message.content.trim();
-  };
-
-  const correctSpelling = async (text) => {
-    const response = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `Correct the spelling mistakes in this text: ${text}`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-      temperature: 0.3,
-      max_tokens: 100,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-    return response.choices[0].message.content.trim();
-  };
-
-  const translate = async (text, language) => {
-    const response = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `Translate this into ${language}: ${text}`,
-        },
-      ],
-      model: "gpt-3.5-turbo",
-      temperature: 0.3,
-      max_tokens: 100,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-    return response.choices[0].message.content.trim();
   };
 
   const handleOnSubmit = async (e) => {
@@ -84,12 +51,20 @@ const App = () => {
     }
     setIsLoading(true);
     try {
-      const detectedLang = await detectLanguage(formData.message);
-      setDetectedLanguage(detectedLang);
-      const corrected = await correctSpelling(formData.message);
-      setCorrectedText(corrected);
-      const translated = await translate(corrected, formData.language);
-      setTranslation(translated);
+      const { languageId, message, modelId } = formData;
+      const data = await fetchData(
+        `history/create?modelId=${modelId}&languageId=${languageId}&input=${message}`
+      );
+
+      if (data.success) {
+        setResult({
+          correctedText: data.data.correctedText,
+          translation: data.data.translatedText,
+          detectedLanguage: data.data.detectedLanguage,
+        });
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
     } catch (err) {
       console.error("Error during translation:", err);
       setError("Something went wrong. Please try again.");
@@ -100,124 +75,88 @@ const App = () => {
 
   const handleCopy = () => {
     navigator.clipboard
-      .writeText(translation)
-      .then(() => displayNotification())
+      .writeText(result.translation)
+      .then(() => {
+        setShowNotification(true);
+        setTimeout(() => setShowNotification(false), 3000);
+      })
       .catch((err) => console.error("Failed to copy: ", err));
-  };
-
-  const displayNotification = () => {
-    setShowNotification(true);
-    setTimeout(() => {
-      setShowNotification(false);
-    }, 3000);
   };
 
   return (
     <div className="container">
       <h1>Translation</h1>
-
       <form onSubmit={handleOnSubmit}>
-        <div className="choices">
-          <input
-            type="radio"
-            id="hindi"
-            name="language"
-            value="Hindi"
-            defaultChecked={formData.language === "Hindi"}
-            onChange={handleInputChange}
-          />
-          <label htmlFor="hindi">Hindi</label>
-
-          <input
-            type="radio"
-            id="spanish"
-            name="language"
-            value="Spanish"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="spanish">Spanish</label>
-
-          <input
-            type="radio"
-            id="english"
-            name="language"
-            value="English"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="english">English</label>
-
-          <input
-            type="radio"
-            id="japanese"
-            name="language"
-            value="Japanese"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="japanese">Japanese</label>
-
-          <input
-            type="radio"
-            id="french"
-            name="language"
-            value="French"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="french">French</label>
+        <div className="models">
+          {options.models.map((model) => (
+            <div key={model.id}>
+              <input
+                type="radio"
+                id={`model-${model.id}`}
+                name="modelId"
+                value={model.id}
+                onChange={handleInputChange}
+              />
+              <label htmlFor={`model-${model.id}`}>{model.name}</label>
+            </div>
+          ))}
         </div>
-
+        <div className="choices">
+          {options.languages.map((language) => (
+            <div key={language.id}>
+              <input
+                type="radio"
+                id={`language-${language.id}`}
+                name="languageId"
+                value={language.id}
+                onChange={handleInputChange}
+              />
+              <label htmlFor={`language-${language.id}`}>{language.name}</label>
+            </div>
+          ))}
+        </div>
         <textarea
           name="message"
           placeholder="Type your message here.."
           onChange={handleInputChange}
         ></textarea>
-
         {error && <div className="error">{error}</div>}
-
         <button type="submit">Translate</button>
       </form>
-
       <div className="translation">
-        {detectedLanguage && (
-          <div className="detected-language">
-            <strong>Detected Language:</strong> {detectedLanguage}
+        {result.detectedLanguage && (
+          <div className="textResult">
+            <strong>Detected Language:</strong> {result.detectedLanguage}
           </div>
         )}
-        <div className="temp">
+        <div className="textResult">
           <h3>Corrected Text</h3>
-          <div className="corrected-text flex-row">
-            {isLoading ? <BeatLoader size={12} color={"red"} /> : correctedText}
-          </div>
+          {isLoading ? <BeatLoader /> : result.correctedText}
         </div>
-        <div className="temp">
-          <h3>Translated Text</h3>
-          <div className="translated-text flex-row">
-            <div>
-              {isLoading ? <BeatLoader size={12} color={"red"} /> : translation}
-            </div>
-
-            <div className="copy-btn" onClick={handleCopy}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                />
-              </svg>
-            </div>
+        <div className="copyDiv">
+          <div className="copy-textResult">
+            <h3>Translated Text</h3>
+            <div>{isLoading ? <BeatLoader /> : result.translation}</div>
+          </div>
+          <div onClick={handleCopy} className="copy-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+              />
+            </svg>
           </div>
         </div>
       </div>
-
-      <div className={`notification ${showNotification ? "active" : ""}`}>
-        Copied to clipboard!
-      </div>
+      {showNotification && <div>Copied to clipboard!</div>}
     </div>
   );
 };
